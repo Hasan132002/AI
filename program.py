9f3c4cf898ec0ffd20351191dbb1c9a84aba1324
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2023-11-12 19:18:50 +0500

    Add files via upload

---
+++ program.py
@@ -0,0 +1,67 @@
+a = float(input("Enter the Percentage: "))
+
+if a >= 90:
+    print("A+")
+elif 80 <= a < 90:
+    print("Ultra A+")
+
+elif 70 <= a < 80:
+    print("A")
+elif 60 <= a < 70:
+    print("B")
+elif 50 <= a < 60:
+    print("C")
+elif 40 <= a < 50:
+    print("D")
+else:
+    print("Fail")
+
+'''
+a=eval(input("Enter the Percentage"))
+if a>=80:
+    print("A+")
+elif a<80 and a>=70:
+    print("A")
+elif a<70 and a>=60:
+    print("B")
+elif a<60 and a>=50:
+    print("C")
+elif a<50 and a>= 40:
+    print("D")
+else:
+    print("Fail")
+
+'''
+'''
+a=input("Enter your email")
+b=input("Enter your password")
+if(a=='hasanraza132002@gmail.com' and b=='hello'):
+    print("Salam")
+else:
+    print("shi se id pass daloo")'''
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
