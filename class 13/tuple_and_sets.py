7d2955199041c342d4fedcfbe2a49bc2e9ca5e0e
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2023-12-17 19:11:01 +0500

    Add files via upload

---
+++ class 13/tuple_and_sets.py
@@ -0,0 +1,64 @@
+ai_tool=('Python','TensorFlow','Pytorch','scikit-learn')
+#first_tool=ai_tool[4]
+#print(first_tool)
+#print(f"My First Tool of Ai is : {first_tool}")
+#ai_tool[0]='Numpy'
+#print(ai_tool)
+
+ai_tools=('Python','TensorFlow','Pytorch',
+          'scikit-learn')
+lang,framwork,framework2,library=ai_tool
+#print('Ai Tool Language is : ', lang)
+#print('Ai Tool Framwork is : ', framwork)
+#for ai in ai_tools:
+#    print(ai)
+
+ai_more_tools=('Pandas','Numpy','Pytorch')
+more=ai_tools+ai_more_tools
+#print(more)
+count_torch=more.count('Pytorch')
+#print(count_torch)
+index_numpy=more.index('Numpy')
+#print(index_numpy)
+
+
+
+lang,framework,framework2,library=ai_tool
+'''print(lang)
+print(framework)
+print(framework2)
+print(library)
+
+'''
+
+
+
+
+'''
+a=[1,2,3,4,5]
+b={'a':'hello'}
+c=(1,2,3)
+print(type(c))
+'''
+
+
+
+ai_tools={'Python','TensorFlow','Pytorch','scikit-learn'}
+
+
+ai_tools.add("Numpy")
+#print(ai_tools)
+
+#for i in ai_tools:
+#    print(i)
+
+ai_tools.update({"Keras","Pandas"})
+
+print(ai_tools)
+#ai_tools.remove('Python')
+#ai_tools.discard('Python')
+b={'Pandas','Numpy','Pytorch','Pandas'}
+#print(ai_tools)
+d=ai_tools.union(b)
+#print(d)
+
